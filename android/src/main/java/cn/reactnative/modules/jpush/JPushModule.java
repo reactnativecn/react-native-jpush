eb448648126ef3a76ad2d8b08ca73284cb6e9949
LvBingru <lvbingru@gmail.com>
2016-01-15 17:32:45 +0800

    bug

--- android/src/main/java/cn/reactnative/modules/jpush/JPushModule.java
+++ android/src/main/java/cn/reactnative/modules/jpush/JPushModule.java
@@ -7,6 +7,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.BuildConfig;
 import android.util.Log;
 
 import com.facebook.react.bridge.Arguments;
